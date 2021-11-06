0f515c0b7d1cded687304fe9d5e4b290d3be1feb
vitor.pinheiro <vitor.pito@gmail.com>
2021-11-06 12:48:08 -0300

    removendo react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home'
 import GlobalStyle from './styles'
 
 export default function App() {
   return (
-    <Router>
-        <GlobalStyle />
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
+    <>
+      <Home />
+      <GlobalStyle />
+    </>
   )
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/icons/logo.svg'
 import { NavBarDiv } from './styles'
@@ -8,9 +7,9 @@ import Options from './Options'
 export default function NavBar() {
   return (
     <NavBarDiv>
-      <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+      <a href="/" onClick={() => window.scrollTo(0, 0)}>
         <Logo width="200" height="120" viewBox="0 0 930.99 466.96"/>
-      </Link>
+      </a>
       <Options />
     </NavBarDiv>
   )
