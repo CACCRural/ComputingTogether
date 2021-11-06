000dbe602baf7483d256d208cf277f042a3f71fe
vitor.pinheiro <vitor.pito@gmail.com>
2021-11-06 03:16:44 -0300

    adicionando arquivos de database

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { Lecture } from '../types'
+import { scheduleData } from './data'
+/*
+* Essa função existe para ser facilmente adicionado um novo local com as informações
+* das palestras
+*/
+export default function getData() {
+  return scheduleData
+}
+
+export function getfirstLecture(dayID: number): Lecture {
+  const day = getData().filter(schedule => schedule.id === dayID)[0]
+  const lectures = day?.lectures
+
+  return lectures[0]
+}
